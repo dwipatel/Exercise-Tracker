5b1e973c6265bf953b30565297d9a31d2bd4baa6
dwipatel <dwipatel00@gmail.com>
2020-06-28 13:03:10 -0400

    Initial commit

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+//creating express server
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+//starts server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
