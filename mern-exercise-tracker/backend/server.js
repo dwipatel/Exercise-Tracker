9d8368e4c0e0a4f3647cf89a9fcc5e2ac0f3f2f4
dwipatel <dwipatel00@gmail.com>
2020-07-01 13:54:18 -0400

    Updated

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -13,11 +15,14 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-
+//once it is opened, it prints this
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
+//when url ends with these, it loads everything from the corresponding file
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 //starts server
 app.listen(port, () => {
