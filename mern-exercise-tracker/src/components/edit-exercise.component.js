1e904883d6f0b6c19a1dea5bff5ef4befc7efde5
dwipatel <dwipatel00@gmail.com>
2020-07-01 18:18:20 -0400

    Finished create exercise and user page

---
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component {
+    render() {
+        return(
+            <div>
+                this is edit exercise component
+            </div>
+        )
+    }
+}
