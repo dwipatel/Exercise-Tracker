62072ab23c3e47e19e9fa565140ef2d8544947bc
dwipatel <dwipatel00@gmail.com>
2020-07-02 18:00:35 -0400

    Connected backend

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class CreateExercises extends Component {
     constructor(props) {
@@ -23,10 +24,16 @@ export default class CreateExercises extends Component {
 
     //this function runs before the component is loaded
     componentDidMount() {
-        this.setState({
-            users:['test user'],
-            username: 'test user',
-        })
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username,
+                    })
+                }
+            })
+        
     }
 
     onChangeUsername(e) {
@@ -53,6 +60,9 @@ export default class CreateExercises extends Component {
         }
       
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
 
         window.location = '/';
     }
