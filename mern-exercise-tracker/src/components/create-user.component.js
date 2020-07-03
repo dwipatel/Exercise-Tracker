2b94e9b83c4f0e996dbce439e239255f57d6fb38
dwipatel <dwipatel00@gmail.com>
2020-07-03 15:15:08 -0400

    Finished components

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -26,7 +26,7 @@ export default class CreateUser extends Component {
         console.log(user);
 
         axios.post('http://localhost:5000/users/add', user)
-            .then(res => console.log(res.data))
+            .then(res => console.log(res.data));
         
         this.setState({
             username: '',
